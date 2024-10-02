7d23f2ce980e5f69bf682cd1cdcc6a740131e6e0
Zagaz <andre_ranulfo@hotmail.com>
2024-10-02 18:58:33 -0300

    Build -> Refactor footer styles to remove text decoration from links

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '00a15c5d966655366372');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '5e41c3966f8e0a7d789a');
