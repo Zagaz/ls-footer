8c945ad6bd22dfff65090e72c17e0632772a788e
Zagaz <andre_ranulfo@hotmail.com>
2024-10-02 15:14:39 -0300

    first commit

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-i18n'), 'version' => '162a72294ee234ae957f');
---
+++ build/view.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '302b2598c9f6e3b76472');
